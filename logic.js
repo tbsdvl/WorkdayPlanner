2cdfdd7b2f1c9da12a483dc87e0a5d2228a45cad
tbsdvl <tristonburns97@gmail.com>
2021-03-08 13:14:42 -0700

    Formatting hours with moment js

--- logic.js
+++ logic.js
@@ -4,27 +4,25 @@ var currentDay = moment();
 $("#currentDay").text(currentDay.format("MMMM Do, YYYY"));
 
 // An array of standard business hours to append to time blocks
-var hoursArray = ["8:00 AM", "9:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM",
-                "3:00 PM", "4:00 PM", "5:00 PM"]
+var hoursArray = ["08:00 AM", "09:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM", "5:00 PM"];
 
 // Create a new list element for each hour and append to the timeblock
-// function stdBusHrs (){
+function stdBusHrs (){
     for(i = 0; i <= hoursArray.length; i++){
     var hoursEl = $("#time-block");
     var hoursListEl = document.createElement("li");
     var textArea = document.createElement("textarea");
+    moment(hoursArray[i], "HHmm A").format("hh:mm A");
     hoursListEl.textContent = hoursArray[i];
     hoursEl.append(hoursArray[i]);
     hoursEl.append(textArea);
     };
+};
 
-    // var currentTime = moment().format('LT');
+stdBusHrs();
 
-    // for(i = 0; i <= hoursArray.length; i++){
-    //     if(hoursArray[i] < currentTime){
-    //         textArea.back
-    //     }
-    // }
-// };
-
-// stdBusHrs();
+// function timeBlockColor() {
+//     for(i = 0; i <= 3; i++){
+//     moment(hoursArray[i], "HHmm A").format("hh:mm A");
+//     hoursArray[i].backgroundColor
+// }
