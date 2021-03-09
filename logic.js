d76db813120ac5812153e332076979c4493fb54d
tbsdvl <tristonburns97@gmail.com>
2021-03-08 21:14:17 -0700

    Adding submit buttons

--- logic.js
+++ logic.js
@@ -9,20 +9,60 @@ var hoursArray = ["08:00 AM", "09:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "1
 // Create a new list element for each hour and append to the timeblock
 function stdBusHrs (){
     for(i = 0; i <= hoursArray.length; i++){
-    var hoursEl = $("#time-block");
+    var hoursEl = $("#time-list");
     var hoursListEl = document.createElement("li");
     var textArea = document.createElement("textarea");
+    var saveBtn = document.createElement("button");
     moment(hoursArray[i], "HHmm A").format("hh:mm A");
     hoursListEl.textContent = hoursArray[i];
+    saveBtn.textContent = "Submit";
+    textArea.setAttribute("id", "text")
+    saveBtn.setAttribute("id", "submit")
     hoursEl.append(hoursArray[i]);
+    hoursEl.append(saveBtn);
     hoursEl.append(textArea);
     };
 };
 
+// Apply the hours text to my document
 stdBusHrs();
 
-// function timeBlockColor() {
-//     for(i = 0; i <= 3; i++){
-//     moment(hoursArray[i], "HHmm A").format("hh:mm A");
-//     hoursArray[i].backgroundColor
-// }
+// var saveBtn = document.createElement("button");
+// var hoursListEl = document.createElement("li");
+// hoursListEl.append(saveBtn);
+
+var currentTime = moment();
+// var testTime = moment('12:00:00', "HHmm A");
+
+// Set background color of textareas to different colors that correspond to the time of day
+function timeBlockColor() {
+    var timeBlock = $("#text");
+    for(i = 0; i <= hoursArray.length; i++){
+    if(moment(hoursArray[i], "HHmm A").isBefore(moment())){
+        timeBlock.css("background-color", "grey");
+        };
+    };
+};
+
+timeBlockColor();
+
+function submitButton() {
+var saveBtn = $("#submit");
+
+saveBtn.addEventListener("click", function(){
+    localStorage($("#text").value);
+    });
+};
+
+submitButton();
+
+// var handleFormSubmit = function (event) {
+//     event.preventDefault();
+  
+//     var nameInput = nameInputEl.val();
+//     var dateInput = dateInputEl.val();
+  
+//     if (!nameInput || !dateInput) {
+//       console.log('You need to fill out the form!');
+//       return;
+//     }
